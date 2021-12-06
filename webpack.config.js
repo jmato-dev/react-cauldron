b06b5c127d2ed942090f5b28bf86f403e1c6e1cc
Joaquín Mato <jmato.dev@gmail.com>
2021-12-06 19:18:44 -0300

    chore: put production and development builds in different directories

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+module.exports = function exports(env, argv) {
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, `dist/${argv.mode}`),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html'),
-    }),
-  ],
+  };
 };
