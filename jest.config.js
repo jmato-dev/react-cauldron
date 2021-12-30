52e6cd0cefb0ada2e71c5450514d93f526682754
Joaquín Mato <jmato.dev@gmail.com>
2021-12-30 13:53:22 -0300

    chore: setup jest asset managment

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/jestSetup.js'],
+  moduleNameMapper: {
+    '\\.module.css$': 'identity-obj-proxy',
+    '\\.css$': '<rootDir>/jest/mocks/styleMock.js',
+    '\\.(gif|jpg|png|webp|svg|eot|otf|ttf|woff|woff2)$':
+      '<rootDir>/jest/mocks/fileMock.js',
+  },
 };
